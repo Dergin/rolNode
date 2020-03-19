8f2db7afe82f6232883d3f963732705c5ce5401f
Adrian <adri_maga91@hotmail.com>
2020-03-20 00:29:46 +0100

    Revert "add"
    This reverts commit 0656229b39183aa4de7787704a3aed840685f742.

--- server/index.js
+++ server/index.js
@@ -7,30 +7,7 @@ var server = require('http').Server(app);
 //Llamamos a socket.io pasandole el server para que escuche
 var io = require('socket.io')(server);
 const PORT = process.env.PORT || 5001;
-const WebSocket = require('ws');
- 
-const wss = new WebSocket.Server({
-  port: 8080,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
-    // should not be compressed.
-  }
-});
+
 //Usamos el midelware para carjar los html de la pagina de cliente que seras estativos
 app.use(express.static('client'));
 //Ejemplo de hola mundo
